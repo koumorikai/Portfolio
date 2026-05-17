668cc9e7aa3f30711c6a7333f8ece2e0d72b3fde
markelxvv <ankorukai@gmail.com>
2026-05-17 13:49:04 +0500

    fix: add deployment workflow and fix base path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/config-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Portfolio/', // ОБЯЗАТЕЛЬНО: название твоего репозитория с косыми чертами
 })
